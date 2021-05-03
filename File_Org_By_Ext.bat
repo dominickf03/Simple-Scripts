13c773c6307c1fce8ea119de6e091b37264a83b0
dominickf03 <dominickf03@users.noreply.github.com>
2021-05-03 14:18:09 -0400

    Create File_Org_By_Ext.bat

---
+++ File_Org_By_Ext.bat
@@ -0,0 +1,22 @@
+REM Simple File Organizer That Sorts Every File Besides Shortcuts, Bat files, and VBS files
+@setlocal & @(for %%I in (*.*) do @set /a ext[%%~xI] += 0) & set ext[ > txt.lnk
+mkdir Files
+type txt.lnk
+cscript rp.vbs "txt.lnk" "ext[." ""
+cscript rp.vbs "txt.lnk" "]=0" ""
+cls
+@echo off
+:moveFiles
+for /f "tokens=*" %%a in ('type txt.lnk') do (
+	set line=%%a
+	if not "%%a" == "bat" (
+		if not "%%a" == "vbs" (
+ 			if not "%%a" == "lnk" (
+				if not "%%a" == "ext[" (
+		echo %%a >> Files\.folders.txt
+		mkdir Files\%%a
+		move "*.%%a" Files\%%a\
+				)	)
+			)
+		) 
+	)
