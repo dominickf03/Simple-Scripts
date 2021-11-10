bf0cde63f3611a16e30b65dd5c97a15e9256ba6e
Dominick Ford <dominickf03@users.noreply.github.com>
2021-11-10 13:36:42 -0500

    Update Arp-Bypass.bat

--- Bypass_Hacking_Attacks/Bypass_arp_attacks/Simple_Bypass/Arp-Bypass.bat
+++ Bypass_Hacking_Attacks/Bypass_arp_attacks/Simple_Bypass/Arp-Bypass.bat
@@ -1,1 +1,24 @@
-
+@echo off
+:top
+cls
+echo Bypass ARP Attacks
+echo.
+echo    Enter the IP address of your router.
+set /p iplol=Example(192.168.1.1) :
+echo %iplol%
+set ipvr=%iplol%
+:iptyp
+echo.
+echo    Enter the mac address of your ip address.
+set /p maclol=Example(00-00-00-00-00-00) :
+echo %maclol%
+set mcvr=%maclol%
+:done
+echo.
+echo Please verify that your routers ip is %ipvr% and your routers mac address is %mcvr%
+pause
+echo.
+netsh interface ipv4 add neighbors "Wi-Fi" %ipvr% %mcvr%
+if %errorlevel%==1 echo error!!! && echo.
+if %errorlevel%==0 echo Successfully changed interface router information!!! && echo.
+pause
